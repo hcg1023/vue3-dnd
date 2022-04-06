337e76086e14fc784d1575c2324596cbf74c44a6
郝晨光 <2293885211@qq.com>
2022-04-06 17:56:49 +0800

    docs: github page

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,6 +2,6 @@ import { createRouter, createWebHistory } from 'vue-router'
 import { routes } from './routes'
 
 export default createRouter({
-	history: createWebHistory(),
+	history: createWebHistory(import.meta.env.BASE_URL),
 	routes,
 })
