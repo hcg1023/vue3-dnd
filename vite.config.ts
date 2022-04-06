3f25938bba6baeb2d7657d9502f0c7b0dfdf6fc7
郝晨光 <2293885211@qq.com>
2022-04-06 17:48:12 +0800

    ci: add github pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,31 +3,13 @@
 import { defineConfig } from 'vite'
 import * as path from 'path'
 import vue from '@vitejs/plugin-vue'
-import dts from 'vite-plugin-dts'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [
-		vue(),
-		dts({
-			root: path.resolve(__dirname, './lib'),
-			outputDir: path.resolve(__dirname, '/dist'),
-		}),
-	],
+	base: process.env.NODE_ENV === 'production' ? '/vue3-dnd' : '/',
+	plugins: [vue()],
 	build: {
-		lib: {
-			entry: path.resolve(__dirname, './lib/index.ts'),
-			formats: ['cjs', 'es'],
-			fileName: format => `${format}/index.js`,
-		},
-		rollupOptions: {
-			external: [
-				'@react-dnd/invariant',
-				'@react-dnd/shallowequal',
-				'dnd-core',
-				'fast-deep-equal',
-			],
-		},
+		outDir: 'dist-docs',
 	},
 	resolve: {
 		alias: {
