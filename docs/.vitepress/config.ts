74bb89b57779adc606cf00efc26370061e4591d7
郝晨光 <2293885211@qq.com>
2022-04-24 10:26:35 +0800

    docs: add algolia

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -28,6 +28,11 @@ export default defineConfig({
 		},
 	},
 	themeConfig: {
+		algolia: {
+			appId: 'MPEA5OIOBN',
+			apiKey: '4731e5999d5e86ca8daf77c77a921adc',
+			indexName: 'vue3-dnd',
+		},
 		locales: {
 			'/': {
 				// 多语言下拉菜单的标题
