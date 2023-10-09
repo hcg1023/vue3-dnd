a83307e0e4d3a7a9ac249360a22162b753fbf893
haochenguang <2293885211@qq.com>
2023-10-09 18:50:46 +0800

    fix: update DndContextSymbol to Symbol.for

--- packages/vue3-dnd/src/core/DndContext.ts
+++ packages/vue3-dnd/src/core/DndContext.ts
@@ -2,7 +2,7 @@ import { inject, InjectionKey, provide } from 'vue-demi'
 import type { DragDropManager } from 'dnd-core'
 
 export const DndContextSymbol: InjectionKey<DragDropManager> =
-  Symbol('DndContextType')
+  Symbol.for('DndContextType')
 
 export function useDndContextProvider(dndContext: DragDropManager) {
   provide(DndContextSymbol, dndContext)
