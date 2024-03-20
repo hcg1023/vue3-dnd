424be118032f0ee267fff9a5152b94e140c02991
haochenguang <2293885211@qq.com>
2024-03-20 20:20:49 +0800

    feat: test-utils support vue2

--- packages/test-utils/src/utils.ts
+++ packages/test-utils/src/utils.ts
@@ -1,8 +1,16 @@
+import { isVue3, nextTick } from 'vue-demi'
+
 /**
  * A tick that users can use to work through the event queue
  */
 export function tick(): Promise<void> {
-  return new Promise(resolve => {
-    setTimeout(resolve, 0)
-  })
+  return nextTick()
+}
+
+export function resolveProps<T extends Record<string, unknown>>(props: T) {
+  return isVue3 ? props : { props }
+}
+
+export function resolveChildren<T>(children: T) {
+  return isVue3 ? { default: () => children } : children
 }
